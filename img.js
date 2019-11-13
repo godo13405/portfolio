0923b46eccabda1781f5794c0f0f97913da38061
Gonçalo Andrade <goncaloandradedovale@gmail.com>
2019-11-13 11:06:56 +0000

    img resizing + webp

--- img.js
+++ img.js
@@ -1,12 +1,32 @@
-const imagemin = require("imagemin");
-const webp = require("imagemin-webp");
+const sharp = require("sharp"),
+  fs = require("fs"),
+  path = require("path"),
+  params = {
+    src: "./content/img/post",
+    width: 1000,
+    height: 1000
+  };
 
-imagemin(["content/img/*.png"], "content/img", {
-  use: [
-    webp({
-      quality: 75
-    })
-  ]
-}).then(function() {
-  console.log("Images converted!");
+function resizeimg(params, cb) {}
+
+fs.readdir(params.src, function(err, files) {
+  files = files.filter(el => /\.(png|jpg|jpeg)$/.test(el));
+  files.forEach(function(file) {
+    const imgPath = path.join(
+      params.src,
+      "/",
+      file
+        .split(".")
+        .slice(0, -1)
+        .join(".")
+    );
+    sharp(imgPath)
+      .resize(params.width, params.height)
+      .toFile(imgPath, (err, info) => {
+        console.log(imgPath + " resized");
+      });
+    sharp(imgPath).toFile(`${imgPath}.webp`, (err, info) => {
+      console.log(imgPath + " to WEBP");
+    });
+  });
 });
